1da35a46490811459094651c99230b5ccca5edf5
Rengoan <91058472+Rengoan@users.noreply.github.com>
2022-03-02 10:33:31 +0100

    123

---
+++ SweetBakeryClase/src/main/java/com/sweetbakery/domain/Cliente.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sweetbakery.domain;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Lukelar
+ */
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Cliente.findAll", query = "SELECT c FROM Cliente c"),
+    @NamedQuery(name = "Cliente.findByIdcliente", query = "SELECT c FROM Cliente c WHERE c.idcliente = :idcliente"),
+    @NamedQuery(name = "Cliente.findByNombre", query = "SELECT c FROM Cliente c WHERE c.nombre = :nombre"),
+    @NamedQuery(name = "Cliente.findByApellido", query = "SELECT c FROM Cliente c WHERE c.apellido = :apellido"),
+    @NamedQuery(name = "Cliente.findByTelefono", query = "SELECT c FROM Cliente c WHERE c.telefono = :telefono"),
+    @NamedQuery(name = "Cliente.findByCorreo", query = "SELECT c FROM Cliente c WHERE c.correo = :correo"),
+    @NamedQuery(name = "Cliente.findByUsuario", query = "SELECT c FROM Cliente c WHERE c.usuario = :usuario"),
+    @NamedQuery(name = "Cliente.findByPass", query = "SELECT c FROM Cliente c WHERE c.pass = :pass"),
+    @NamedQuery(name = "Cliente.findByDireccion", query = "SELECT c FROM Cliente c WHERE c.direccion = :direccion")})
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    private Integer idcliente;
+    @Size(max = 45)
+    private String nombre;
+    @Size(max = 45)
+    private String apellido;
+    @Size(max = 45)
+    private String telefono;
+    @Size(max = 45)
+    private String correo;
+    @Size(max = 45)
+    private String usuario;
+    @Size(max = 45)
+    private String pass;
+    @Size(max = 70)
+    private String direccion;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cliente")
+    private Collection<Pedido> pedidoCollection;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public Integer getIdcliente() {
+        return idcliente;
+    }
+
+    public void setIdcliente(Integer idcliente) {
+        this.idcliente = idcliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Collection<Pedido> getPedidoCollection() {
+        return pedidoCollection;
+    }
+
+    public void setPedidoCollection(Collection<Pedido> pedidoCollection) {
+        this.pedidoCollection = pedidoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idcliente != null ? idcliente.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) object;
+        if ((this.idcliente == null && other.idcliente != null) || (this.idcliente != null && !this.idcliente.equals(other.idcliente))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sweetbakery.domain.Cliente[ idcliente=" + idcliente + " ]";
+    }
+    
+}
---
+++ SweetBakeryClase/src/main/java/com/sweetbakery/domain/Detallespedido.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sweetbakery.domain;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author Lukelar
+ */
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Detallespedido.findAll", query = "SELECT d FROM Detallespedido d"),
+    @NamedQuery(name = "Detallespedido.findByIddetallespedido", query = "SELECT d FROM Detallespedido d WHERE d.iddetallespedido = :iddetallespedido"),
+    @NamedQuery(name = "Detallespedido.findByPrecioVente", query = "SELECT d FROM Detallespedido d WHERE d.precioVente = :precioVente"),
+    @NamedQuery(name = "Detallespedido.findByCantidad", query = "SELECT d FROM Detallespedido d WHERE d.cantidad = :cantidad")})
+public class Detallespedido implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    private Integer iddetallespedido;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "precio_vente")
+    private Double precioVente;
+    private Integer cantidad;
+    @JoinColumn(name = "pedido_idpedido", referencedColumnName = "idpedido")
+    @ManyToOne(optional = false)
+    private Pedido pedido;
+    @JoinColumn(name = "producto_idproducto", referencedColumnName = "idproducto")
+    @ManyToOne(optional = false)
+    private Producto producto;
+
+    public Detallespedido() {
+    }
+
+    public Detallespedido(Integer iddetallespedido) {
+        this.iddetallespedido = iddetallespedido;
+    }
+
+    public Integer getIddetallespedido() {
+        return iddetallespedido;
+    }
+
+    public void setIddetallespedido(Integer iddetallespedido) {
+        this.iddetallespedido = iddetallespedido;
+    }
+
+    public Double getPrecioVente() {
+        return precioVente;
+    }
+
+    public void setPrecioVente(Double precioVente) {
+        this.precioVente = precioVente;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (iddetallespedido != null ? iddetallespedido.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Detallespedido)) {
+            return false;
+        }
+        Detallespedido other = (Detallespedido) object;
+        if ((this.iddetallespedido == null && other.iddetallespedido != null) || (this.iddetallespedido != null && !this.iddetallespedido.equals(other.iddetallespedido))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.sweetbakery.domain.Detallespedido[ iddetallespedido=" + iddetallespedido + " ]";
+    }
+    
+}
